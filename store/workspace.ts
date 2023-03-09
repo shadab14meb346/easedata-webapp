7c55132b918c4dffdcdda21100f8ab25c7f45153
shadab <shadab14meb346@gmail.com>
2023-03-09 13:48:53 +0530

    persist the selected workspace using localstore

--- store/workspace.ts
+++ store/workspace.ts
@@ -1,4 +1,5 @@
 import { makeVar, useReactiveVar } from '@apollo/client';
+import { useEffect } from 'react';
 import { WorkspaceType } from 'types/workspace';
 
 export interface WorkspaceStore {
@@ -9,14 +10,24 @@ export const workspaceStoreVar = makeVar<WorkspaceStore>({
   selectedWorkspace: null,
 });
 
+const persistWorkspaceStore = (data: WorkspaceStore) => {
+  localStorage.setItem('workspaceStore', JSON.stringify(data));
+};
 export const updateWorkspaceStore = (data: WorkspaceStore) => {
   workspaceStoreVar({
     ...workspaceStoreVar(),
     ...data,
   });
+  persistWorkspaceStore(workspaceStoreVar());
 };
 
 export const useWorkspaceStore = () => {
   const reactiveExampleVar = useReactiveVar(workspaceStoreVar);
+  useEffect(() => {
+    const persistedWorkspaceStore = localStorage.getItem('workspaceStore');
+    if (persistedWorkspaceStore) {
+      updateWorkspaceStore(JSON.parse(persistedWorkspaceStore));
+    }
+  }, []);
   return reactiveExampleVar;
 };
