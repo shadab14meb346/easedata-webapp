445b3345d95f2b140eb5d79d10907ba3f9d1ea52
Shadab <shadab14meb346@gmail.com>
2023-04-17 17:30:49 +0530

    ms office xl sheet oauth flow (#37)

--- src/components/Dashboard/DataSources/index.tsx
+++ src/components/Dashboard/DataSources/index.tsx
@@ -2,9 +2,10 @@ import { Button, Box, Typography } from '@mui/material';
 
 import { useStyles } from './useStyles';
 import useAuth from '@utils/useAuth';
-import { getHubSpotOauthURL } from 'src/utils';
+import { getHubSpotOauthURL, getMSOfficeOauthURL } from 'src/utils';
 import { useWorkspaceStore } from '@store/workspace';
 import HubSpotIcon from '@public/hubspot-icon.svg';
+import MSOfficeXLIcon from '@public/ms-office-xl-icon.svg';
 
 const DataSources = () => {
   const classes = useStyles();
@@ -12,7 +13,6 @@ const DataSources = () => {
   const { selectedWorkspace } = useWorkspaceStore();
   return (
     <div className={classes.main}>
-      <Typography variant="body1">Import Data From</Typography>
       <Box marginTop={1}>
         <Button
           variant="outlined"
@@ -24,6 +24,19 @@ const DataSources = () => {
           startIcon={<HubSpotIcon style={{ width: 24, height: 24 }} />}
         >
           HubSpot
+        </Button>
+
+        <Button
+          variant="outlined"
+          href={getMSOfficeOauthURL({
+            jwt: authState?.token as string,
+            workspaceId: selectedWorkspace?.id as string,
+          })}
+          target="_blank"
+          startIcon={<MSOfficeXLIcon style={{ width: 24, height: 24 }} />}
+          sx={{ ml: 2 }}
+        >
+          Ms Office XL
         </Button>
       </Box>
     </div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,18 @@
 import HubSpotIcon from '../../public/hubspot-icon.svg';
 const scopes =
   'crm.lists.read%20crm.objects.contacts.read%20crm.schemas.custom.read%20crm.objects.custom.read%20crm.schemas.contacts.read%20crm.objects.companies.read%20crm.schemas.companies.read%20crm.schemas.deals.read%20crm.objects.owners.read%20crm.objects.quotes.read';
+const MS_OFFICE_SCOPES = encodeURI(
+  ['offline_access', 'Files.ReadWrite'].join(' ')
+);
 
-type GetHubSpotOauthURLInput = {
+type OauthURLInput = {
   jwt: string;
   workspaceId: string;
 };
 export const getHubSpotOauthURL = ({
   jwt,
   workspaceId,
-}: GetHubSpotOauthURLInput): string => {
+}: OauthURLInput): string => {
   const state = JSON.stringify({
     token: jwt,
     workspaceId,
@@ -17,11 +20,24 @@ export const getHubSpotOauthURL = ({
   const INSTALL_OAUTH_URL = `https://app.hubspot.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_HUB_SPOT_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_HUB_SPOT_REDIRECT_URI}&scope=${scopes}&state=${state}`;
   return INSTALL_OAUTH_URL;
 };
+export const getMSOfficeOauthURL = ({
+  jwt,
+  workspaceId,
+}: OauthURLInput): string => {
+  const state = JSON.stringify({
+    token: jwt,
+    workspaceId,
+  });
+  const INSTALL_OAUTH_URL = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${process.env.NEXT_PUBLIC_MS_OFFICE_CLIENT_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_MS_OFFICE_REDIRECT_URI}&response_mode=query&scope=${MS_OFFICE_SCOPES}&state=${state}`;
+  return INSTALL_OAUTH_URL;
+};
 
 export const getDataSourceIcon = (dataSourceType: string) => {
   switch (dataSourceType) {
     case 'HUB_SPOT':
       return '/hubspot-icon.svg';
+    case 'MS_OFFICE_SHEET':
+      return '/ms-office-xl-icon.svg';
     default:
       return '';
   }
